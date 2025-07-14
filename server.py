eded80fccecbc9aa82ee8bbbe9fe152db951cc93
AIDEVEYSBD <jai.verma2@in.ey.com>
2025-07-15 04:31:30 +0530

    Update server.py

--- server.py
+++ server.py
@@ -813,7 +813,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "server:app", 
         host="0.0.0.0", 
-        port=8001, 
+        port=8000, 
         reload=True,
         access_log=True,
         log_level="info"
